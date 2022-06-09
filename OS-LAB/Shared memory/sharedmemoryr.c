7b7f2c7805edd5a3d9efbdc7febd2cadf1ce18bf
20WH1A1222 <97152848+20WH1A1222@users.noreply.github.com>
2022-06-09 15:19:12 +0530

    Update sharedmemoryr.c

--- OS-LAB/Shared memory/sharedmemoryr.c
+++ OS-LAB/Shared memory/sharedmemoryr.c
@@ -11,7 +11,7 @@ char buff[100];
 int shmid;  
 shmid=shmget((key_t)2345, 1024, 0666);  
 printf("Key of shared memory is %d\n",shmid);  
-shared_memory=shmat(shmid,NULL,0); //process attached to shared memory segment  
+shared_memory=shmat(shmid,NULL,0);   
 printf("Process attached at %p\n",shared_memory);  
 printf("Data read from shared memory is : %s\n",(char *)shared_memory);  
 }  
